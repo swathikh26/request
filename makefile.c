baf33d9e8a3e8964358969a7de8b5cde4ef5c5cc
swathikh26 <94822023+swathikh26@users.noreply.github.com>
2021-12-01 19:36:31 +0530

    Update makefile.c

--- makefile.c
+++ makefile.c
@@ -1,6 +1,8 @@
-big3.exe:big3.o factorial.o
-	gcc -o big3.exe big3.o factorial.o
-big3.o:big3.c
+ABC.exe: main.o big3.o fact.o
+	gcc -o ABC.exe main.o big3.o fact.o
+main.o: main.c
+        gcc -c main.c
+big3.o: big3.c
 	gcc -c big3.c
-factorial.o:factorial.c
-	gcc -c factorial.c
+fact.o: fact.c
+	gcc -c fact.c
